4c78b977a0131a2d40c676867a4baaf732756796
ItaloNicola <italo.niccola@gmail.com>
2018-06-02 20:38:39 +0000

    Clean up windows build script

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@
 cls
 rmdir build /s /q
 mkdir build
-copy lib\GLFW\glfw3.dll build\
 copy src\*.glsl build\
 cl src\main.c /I.\include\ /MD /DWIN32 /EHsc /nologo /link lib\GLFW\glfw3.lib user32.lib gdi32.lib msvcrt.lib shell32.lib /OUT:"build/main_console.exe" /SUBSYSTEM:CONSOLE
 cl src\main.c /I.\include\ /MD /DWIN32 /EHsc /nologo /link lib\GLFW\glfw3.lib user32.lib gdi32.lib msvcrt.lib shell32.lib /OUT:"build/main.exe" /SUBSYSTEM:WINDOWS /ENTRY:"mainCRTStartup"
